f83d040f00c2796f29ce72b8a90799d3ff6cce71
Masato Sean Goda <masatoseangoda@gmail.com>
2021-03-29 20:12:39 -0400

    Initial Changes for Pokedex
    Added the first three Pokemon's into the js.code

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
-alert('Hello world');
-let favoriteFoods= "Pasta";
-document.write(favoriteFoods)
+let pokemonList = [];
+pokemonList = [
+    {
+    name: "Bulbasaur",
+    height:0.7,
+    type:"grass"
+    },
+    {
+      name: "Wartortle" ,
+      height: "1" ,
+      type:"water"
+    },
+    {
+        name: "Beedrill",
+        height: "1",
+        type: "bug"
+    }
+
+]
