b47111b658794795a2b06834d930466a4010bbda
Masato Sean Goda <masatoseangoda@gmail.com>
2021-03-31 12:56:03 -0400

    loop and condition
    Added loop and conditions to my code

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,15 @@
 let pokemonList = [];
 pokemonList = [
-    {
-    name: "Bulbasaur",
-    height:0.7,
-    type:"grass"
-    },
-    {
-      name: "Wartortle" ,
-      height: "1" ,
-      type:"water"
-    },
-    {
-        name: "Beedrill",
-        height: "1",
-        type: "bug"
-    }
-
+    { name: "Bulbasaur", height:0.7, type: ["grass"]},
+    { name: "Wartortle" , height: "1" ,type:["water"]},
+    {name: "Beedrill", height: "1", type: ["bug"]}
 ]
+
+let pokemonSizeThreshold = 0.8;
+
+for( i = 0; i <= pokemonList.length; i++) {
+    let pokemonString = `${pokemonList[i].name} ( Height: ${ pokemonList[i].height} )`;
+    let pokemon = pokemonList[i].height >= pokemonSizeThreshold ? `<p> ${pokemonString} - Wow, that’s big! </p>` : `<p> ${pokemonString} </p>`;
+    document.write(pokemon);
+}
+
