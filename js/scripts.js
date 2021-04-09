957cd5320db1b45dbbff3f083ae08332f82e63c6
Masato Sean Goda <masatoseangoda@gmail.com>
2021-04-09 12:43:07 -0400

    fixed up JS code

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,42 @@
-let pokemonRepositry=(function() {
-let pokemonList = [];
-let pokemon= "";
-let pokemonList = [
-    { 
-        name: "Bulbasaur", 
-    height:"0.7", 
+let pokemonRepository=(function() {
+    let pokemonList = [
+    { name: "Bulbasaur",
+    height:0.7,
     type: "grass"
-},
-    { 
-        name: "Wartortle" , 
+    },
+    { name: "Wartortle",
     height: "1" ,
     type: "water"
-},
-    {
-        name: "Beedrill",
-     height: "1", 
-     type: "bug"
+    },
+    {name: "Beedrill",
+    height: "1",
+    type: "bug"
     }
-];
-
-function add(pokemon){
+    ];
+    
+    function add(pokemon){
     if (typeof pokemon === 'object' && typeof pokemon !== null) {
-        pokemonList.push(item);
-      } else {
-        console.log('you need an object');
-      }
-}
-
-function getAll(){
+    pokemonList.push(item);
+    } else {
+    console.log('you need an object');
+    }
+    }
+    
+    function getAll(){
     return pokemonList;
-}
-
-return {
+    }
+    
+    return {
     add: add,
     getAll: getAll
-};
-})();
-
-pokemonRepository.getAll().forEach(function(pokemon) {
-    if(pokemon.height > .5){
-    document.write('${(pokemon.name)} - Height: ${pokemon.height}cm - Type: ${pokemon.type} - Now that is large! <br/> <br/>');
+    };
+    })();
+    
+    pokemonRepository.getAll().forEach(function(pokemon) {
+    if(pokemon.height > 1){
+    document.write(`${(pokemon.name)} - Height: ${pokemon.height}cm - Type: ${pokemon.type} - Now that is large! <br/> <br/>`);
     }
     else{
-    document.write('${(pokemon.name)} - Height: ${pokemon.height}cm - Type: ${pokemon.type} <br/> <br/>');
+    document.write(`${(pokemon.name)} - Height: ${pokemon.height}cm - Type: ${pokemon.type} <br/> <br/>`);
     }
     })
-
-function findgrass(pokemonList){
-    if(pokemonList.type === "grass"){
-        return pokemon
-    }
-}
-
-pokemonRepository.getAll().forEach(loopFunction);
-
-let grass=pokemonRepository.getAll().filter(findGrass);
-
-console.log(grass)
