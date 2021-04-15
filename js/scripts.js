e9bae109bae41a4d7ca357790575e58dc8168d0f
Masato Sean Goda <masatoseangoda@gmail.com>
2021-04-15 14:48:17 -0400

    1.6 updates addListItem

--- js/scripts.js
+++ js/scripts.js
@@ -5,27 +5,46 @@ let pokemonRepository=(function() {
     type: "grass"
     },
     { name: "Wartortle",
-    height: "1" ,
+    height: 1,
     type: "water"
     },
     {name: "Beedrill",
-    height: "1",
+    height: 1,
     type: "bug"
     }
     ];
     
-    function add(pokemon){
+    function addListItem(pokemon){
     if (typeof pokemon === 'object' && typeof pokemon !== null) {
     pokemonList.push(item);
     } else {
     console.log('you need an object');
     }
-    }
-    
+
     function getAll(){
     return pokemonList;
     }
-    
+
+
+    function addListItem(pokemon){
+        let pokemonList = document.querySelector(".pokemon-list");
+        let listpokemon = document.createElement("li");
+        let button = document.createElement("button");
+        button.innerText = pokemon.name;
+        button.classList.add("button-class");
+        listpokemon.appendChild(button);
+        pokemonList.appendChild(listpokemon);
+        pokedexListener(button, pokemon);
+    }
+      }
+
+      function showDetails(pokemon) {
+        console.log(pokemon);
+      }
+
+
+
+
     return {
     add: add,
     getAll: getAll
@@ -33,10 +52,5 @@ let pokemonRepository=(function() {
     })();
     
     pokemonRepository.getAll().forEach(function(pokemon) {
-    if(pokemon.height > 1){
-    document.write(`${(pokemon.name)} - Height: ${pokemon.height}cm - Type: ${pokemon.type} - Now that is large! <br/> <br/>`);
-    }
-    else{
-    document.write(`${(pokemon.name)} - Height: ${pokemon.height}cm - Type: ${pokemon.type} <br/> <br/>`);
-    }
-    })
+    pokemonRepository.addListItem(pokemon);
+    });
